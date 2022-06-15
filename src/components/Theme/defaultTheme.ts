86d5ba0a9006296170f2fde2a8292458132047e8
sergeysychevskiy <sergey.sychevskiy@stryber.com>
2022-06-15 16:10:15 +0300

    - fixed Form reset - changed primary light color (used from the web)

--- src/components/Theme/defaultTheme.ts
+++ src/components/Theme/defaultTheme.ts
@@ -11,13 +11,13 @@ export const defaultTheme = {
   },
   primary: {
     main: '#003CB8',
-    light: '#C2C9F1',
+    light: '#E7EAF9',
     dark: '#001D9A',
     contrast: '#FFF',
   },
   secondary: {
     main: '#58329A',
-    light: '#C2C9F1',
+    light: '#EDE7F4',
     dark: '#3D2181',
     contrast: '#FFF',
   },
