256e02fd45d3cc7741352a0abf56d7552f6b8761
vladbelcar <vladstart1@hotmail.com>
2023-01-03 12:26:51 +0200

    GV2-64 | Fixed fill for PaymentMethodIcon on web

--- src/components/Icons/PaymentMethodIcon.tsx
+++ src/components/Icons/PaymentMethodIcon.tsx
@@ -206,7 +206,7 @@ const PaymentMethodIcon: React.FC<IPaymentMethodIconProps> = ({
   ...rest
 }) => (
   <View style={{width, height}}>
-    <Svg {...rest} width="100%" height="100%" viewBox="0 0 34 24">
+    <Svg fill="none" {...rest} width="100%" height="100%" viewBox="0 0 34 24">
       {paymentMethodVariants[variant]}
     </Svg>
   </View>
