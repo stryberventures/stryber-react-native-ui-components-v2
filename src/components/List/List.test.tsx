ecd3fd65f67a5a509b07edf91d9b0ca678f76a6c
vladbelcar <vladstart1@hotmail.com>
2023-06-21 15:02:20 +0300

    GV2-175 | Fixed tests

--- src/components/List/List.test.tsx
+++ src/components/List/List.test.tsx
@@ -1,42 +1,50 @@
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react-native';
-import {Text} from 'react-native';
-import List, {IListItem} from '.';
+import List, {ListItem, ListItemText} from './index';
 import * as Icons from '../Icons';
 
 const listItemTestID = 'test-list-item';
 const leftContentTestID = 'test-left-content';
 const rightContentTestID = 'test-right-content';
-
 const onPress = jest.fn();
 
-const listItems: IListItem[] = [
-  {
-    title: 'One-line Item',
-    subtitle: 'Secondary Text',
-    leftContent: <Icons.InfoIcon fill="#667085" testID={leftContentTestID} />,
-    rightContent: <Text testID={rightContentTestID}>01</Text>,
-    testID: listItemTestID,
-    onPress,
-  },
-];
+const firstItem = {
+  title: 'One-line Item',
+  subtitle: 'Secondary Text',
+  leftContent: <Icons.CalendarIcon testID={leftContentTestID} />,
+  rightContent: <Icons.InfoIcon testID={rightContentTestID} />,
+  testID: listItemTestID,
+  onPress,
+  size: 'medium',
+};
 
-const firstItem = listItems[0];
+const ComponentWithList = () => (
+  <List>
+    <ListItem
+      rightContent={firstItem.rightContent}
+      leftContent={firstItem.leftContent}
+      onPress={firstItem.onPress}
+      testID={firstItem.testID}
+    >
+      <ListItemText primary={firstItem.title} secondary={firstItem.subtitle} />
+    </ListItem>
+  </List>
+);
 
 it('renders title and subtitle', () => {
-  const {getByText} = render(<List listItems={listItems} />);
+  const {getByText} = render(<ComponentWithList />);
   getByText(firstItem.title);
   getByText(firstItem.subtitle!);
 });
 
 it('renders left and right content', () => {
-  const {getByTestId} = render(<List listItems={listItems} />);
+  const {getByTestId} = render(<ComponentWithList />);
   getByTestId(leftContentTestID);
   getByTestId(rightContentTestID);
 });
 
 it('fires onPress', () => {
-  const {getByTestId} = render(<List listItems={listItems} />);
+  const {getByTestId} = render(<ComponentWithList />);
   fireEvent.press(getByTestId(listItemTestID));
   expect(onPress).toHaveBeenCalled();
 });
